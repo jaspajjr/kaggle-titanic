3b3fc7d6d76e228841ae7192dbc63aa1ec2a1733
John Carney <jaspajjr@gmail.com>
2018-01-01 21:32:23 +0000

    Changed prepare_data function to add OneHot.

--- src/main.py
+++ src/main.py
@@ -3,28 +3,27 @@ import preparation
 import exploration
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+
 
 def main():
     df = utils.get_data()
     data = preparation.prepare_data(df)
     print data.head()
+
     target = data[['Survived']].copy()
-    del data['Survived']
+    data = data.drop(columns=['Survived'], axis=1)
 
-    for column in ['Age', 'Fare', 'Parch', 'Pclass',
-            'SibSp' , 'Survived']:
-        exploration.distribution_visualization(df[column])
-
-    X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.33, random_state=42)
-    
+    X_train, X_test, y_train, y_test = train_test_split(
+            data,
+            target,
+            test_size=0.33,
+            random_state=42)
     rfc = model_fitting(X_train, y_train)
 
     print rfc.score(X_test, y_test)
 
 
 def model_fitting(X_train, y_train):
-    print y_train.info()
     rfc = RandomForestClassifier()
     rfc.fit(X_train, y_train)
 
--- src/preparation.py
+++ src/preparation.py
@@ -1,23 +1,31 @@
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+import numpy as np
+import pandas as pd
+
 
 def generate_surname(df):
+    '''
+    Gets the surname from the name column.
+    '''
     df = df.copy()
     df['surname'] = df['Name'].apply(lambda x: x.split(',')[0])
     return df
 
+
 def prepare_data(df):
     df = generate_surname(df)
     data = df[['Age', 'Embarked', 'Fare', 'Parch', 'PassengerId', 'Pclass',
-               'Sex', 'SibSp' , 'Survived' ,'surname']].copy()
+               'Sex', 'SibSp', 'Survived', 'surname']].copy()
+
+    data = pd.concat(
+        [df, pd.get_dummies(df['Embarked']), pd.get_dummies(df['Parch']),
+         pd.get_dummies(df['Pclass']), pd.get_dummies(df['Sex'])], axis=1) \
+         .drop(columns=['Embarked', 'Parch', 'Sex', 'Pclass', 'Cabin', 'Name',
+                        'Ticket', 'surname'], axis=1)
 
 
-    data['Sex'] = LabelEncoder().fit_transform(data['Sex'])
-    embarkation_dict = {'S': 1, 'C': 2, 'Q':3, 0: 0 }
-    data['Embarked'] = data['Embarked'] \
-            .fillna(0)\
-            .apply(lambda x: embarkation_dict[x])
-    data['Age'] = data['Age'].fillna(0)
-    data['surname'] = LabelEncoder().fit_transform(data['surname'])
+    # data['Sex'] = LabelEncoder().fit_transform(data['Sex'])
+    # data['Age'] = np.log(data['Age'].fillna(0))
     data = data.dropna()
 
     return data
